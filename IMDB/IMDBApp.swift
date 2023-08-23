17844ecc8ecd34d47a58ff8a6580830adeabf09e
Urvashi <urvashi.gupta@housing.com>
2023-08-23 14:46:59 +0530

    Initial Commit

---
+++ IMDB/IMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDBApp.swift
+//  IMDB
+//
+//  Created by Urvashi Gupta on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct IMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
